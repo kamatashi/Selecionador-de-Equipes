fe3d6e52b890805499419e158042f5a8d42a72e7
kamatashi <110176533+kamatashi@users.noreply.github.com>
2024-01-17 19:45:52 +0000

    feat: calculateTeams function and removeNumbersFromString function

--- src/components/TeamDistribution/index.jsx
+++ src/components/TeamDistribution/index.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const TeamDistribution = () => {
   const [peopleList, setPeopleList] = useState('');
   const [numTeams, setNumTeams] = useState(0);
   const [teams, setTeams] = useState([]);
+  const [peoplePerTeam, setPeoplePerTeam] = useState(0);
 
   const handlePeopleListChange = (e) => {
     setPeopleList(e.target.value);
@@ -13,13 +14,32 @@ const TeamDistribution = () => {
     setNumTeams(e.target.value);
   };
 
+  function removeNumbersFromString(inputString) {
+    // Utiliza uma expressão regular para remover os números
+    return inputString.replace(/\d/g, '');
+  }
+  
+  const calculateTeams = () => {
+    const peopleWtthoutNumbers = removeNumbersFromString(peopleList); // VEJA ISSO AQUI, DEPOIS ////////
+    const peopleArray = peopleWtthoutNumbers.split(". ").map((name) => name.trim());
+    const maxPeoplePerTeam = Math.ceil(peopleArray.length / 7); // Pode ajustar o limite conforme necessário
+    setPeoplePerTeam(maxPeoplePerTeam);
+
+    // Calcula o número de times com base no limite de pessoas por time
+    const calculatedNumTeams = Math.ceil(peopleArray.length / maxPeoplePerTeam);
+    setNumTeams(calculatedNumTeams);
+  };
+
   const distributeTeams = () => {
-    const peopleArray = peopleList.split('. ').map((name) => name.trim());
+    const peopleArray = peopleList.split(". ").map((name) => name.trim());
     const shuffledPeople = peopleArray.sort(() => Math.random() - 0.5);
 
     const distributedTeams = Array.from({ length: numTeams }, (_, index) => ({
       teamNumber: index + 1,
-      members: shuffledPeople.slice(index * Math.ceil(peopleArray.length / numTeams), (index + 1) * Math.ceil(peopleArray.length / numTeams)),
+      members: shuffledPeople.slice(
+        index * Math.ceil(peopleArray.length / numTeams),
+        (index + 1) * Math.ceil(peopleArray.length / numTeams),
+      ),
     }));
 
     setTeams(distributedTeams);
@@ -30,12 +50,17 @@ const TeamDistribution = () => {
       <h1>Destribuição de times</h1>
       <label>
         Lista com os participantes:
-        <input type="text" value={peopleList} onChange={handlePeopleListChange} />
+        <input
+          type="text"
+          value={peopleList}
+          onChange={handlePeopleListChange}
+        />
       </label>
       <br />
       <label>
         Número de times:
         <input type="number" value={numTeams} onChange={handleNumTeamsChange} />
+        <button onClick={calculateTeams}>Número ideal de times</button>
       </label>
       <br />
       <button onClick={distributeTeams}>Clique para destribuir</button>
